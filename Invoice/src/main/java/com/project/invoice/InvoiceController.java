b87ed043ce72db2da0c55b7328658c6cb9aedf11
Kien <raikikien@gmail.com>
2021-03-09 17:51:40 +0700

    update

--- Invoice/src/main/java/com/project/invoice/InvoiceController.java
+++ Invoice/src/main/java/com/project/invoice/InvoiceController.java
@@ -1,38 +1,46 @@
 package com.project.invoice;
 
+import java.util.Arrays;
 import java.util.List;
-
+import com.project.invoice.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class InvoiceController {
  
     @Autowired
-    private InvoiceService service;
-     
-  /*  @GetMapping("/users/{userId}/invoices")
-    public ResponseEntity<String> getInvoices(@PathVariable String userId){
-    	ResponseEntity.ok(service.listAll());
-    }*/ 
-    @RequestMapping("/invoices")
-    public String viewHomePage(Model model) {
-        List<Invoice> listInvoices = service.listAll();
-        model.addAttribute("listInvoices", listInvoices);
-         
-        return "index";
+    private InvoiceService service;    
+    
+    @GetMapping(value ="/invoices")
+    public ResponseEntity<List<Invoice>> findAllInvoice() {
+        List<Invoice> invoices = service.listAll();
+        if (invoices.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(invoices, HttpStatus.OK);
     }
+    
+    @PostMapping(value="/invoices/new")
+    public void newInvoice() {
+    	Invoice invoice = new Invoice();
+    }
+    
+    
     @RequestMapping("/new")
     public String showNewInvoicePage(Model model) {
         Invoice invoice = new Invoice();
